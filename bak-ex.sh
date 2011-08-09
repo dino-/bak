4efcc8cb7f53eb75bf3a0b995eb6d7a0a90d726f
Dino Morelli <dino@ui3.info>
2011-08-09 22:11:06 +0000

    Fixed a comment

--- bak-ex.sh
+++ bak-ex.sh
@@ -7,7 +7,7 @@ src=/
 dest=user@domain:/var/local/backup/system
 
 # How long to wait for the backup to finish before failing
-# see man timeout (1), values like 37s, 10m, 1h and 2d
+# see man 1 timeout, values like 37s, 10m, 1h and 2d
 timeoutDuration=2h
 
 
