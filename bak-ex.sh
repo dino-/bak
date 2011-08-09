a389a4dfbf025c583d70d0e18b3598a1e7ba60bd
Dino Morelli <dino@ui3.info>
2011-08-09 14:59:27 +0000

    Added some usage comments

--- bak-ex.sh
+++ bak-ex.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
+# Make sure you comment this to actually perform backups
 dryRun=--dry-run
 
 src=/
 dest=user@domain:/var/local/backup/system
 
+# How long to wait for the backup to finish before failing
 # see man timeout (1), values like 37s, 10m, 1h and 2d
 timeoutDuration=2h
 
