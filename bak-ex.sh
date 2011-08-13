107b309dc8b4efc4cf573ac093ba741ac51e9889
Dino Morelli <dino@ui3.info>
2011-08-13 14:58:57 +0000

    Added package list generation for Arch or Debian
    Renamed the command variable to backupCmd for clarity as well.

--- bak-ex.sh
+++ bak-ex.sh
@@ -6,9 +6,17 @@ dryRun=--dry-run
 src=/
 dest=user@domain:/var/local/backup/system
 
+# for Arch Linux
+pkgListCmd="pacman -Qqe | grep -Fvx \"\$(pacman -Qqm)\" > /var/lib/pacman/installed-packages"
+
+# for Debian
+#pkgListCmd="dpkg --get-selections > /var/lib/dpkg-sel-backup"
+
 # How long to wait for the backup to finish before failing
 # see man 1 timeout, values like 37s, 10m, 1h and 2d
 timeoutDuration=2h
+
+backupCmd="rsync --archive --verbose --delete --delete-excluded --filter '. /etc/bak/bak-system.filter' $dryRun $src $dest 2>&1"
 
 
 log () {
@@ -16,11 +24,15 @@ log () {
 }
 
 
-command="rsync --archive --verbose --delete --delete-excluded --filter '. /etc/bak/bak-system.filter' $dryRun $src $dest 2>&1"
-
 log "Backup started"
-log "$command"
-timeout $timeoutDuration sh -c "$command"
+
+log "Executing command: $pkgListCmd"
+eval $pkgListCmd
+
+log "Executing command: $backupCmd"
+timeout $timeoutDuration sh -c "$backupCmd"
 exitCode=$?
+
 log "Done"
+
 exit $exitCode
