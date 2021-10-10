e4ff25895b849f21a0332e630efaaaef21731dba
Dino Morelli <dino@ui3.info>
2021-10-10 12:44:28 -0400

    OR an alternative installation shape
    This one doesn't try to put anything above the PREFIX. Usually this
    means all below `/usr/` or similar.
    
    Not sure how to handle `/var/lib` things, first-run?

--- install.sh
+++ install.sh
@@ -3,17 +3,16 @@
 version="1.0"
 
 ROOTDIR=${ROOTDIR:-"bak-${version}"}
-PREFIX=${PREFIX:-"$ROOTDIR/usr/local"}
-# unitDir="$PREFIX/lib/systemd/system"
-unitDir="$ROOTDIR/etc/systemd/system"
+PREFIX=${PREFIX:-"$ROOTDIR/usr"}
+unitDir="$PREFIX/lib/systemd/system"
 shareDir="$PREFIX/share/bak"
 
-install -Dm0644 bak-hostname.timer "$unitDir/bak-hostname.timer"
-install -Dm0644 bak@.service "$unitDir/bak@.service"
-install -Dm0754 bak-ex.sh "$PREFIX/bin/bak-$(hostname).sh"
-install -Dm0644 bak-ex.filter "$ROOTDIR/var/lib/bak/bak-$(hostname).filter"
-install -Dm0754 bak-log.sh "$PREFIX/bin/bak-log.sh"
-install -Dm0754 rsync-errors.sh "$PREFIX/bin/rsync-errors.sh"
+install -Dm0644 bak-hostname.timer "${unitDir}/bak-hostname.timer"
+install -Dm0644 bak@.service "${unitDir}/bak@.service"
+install -Dm0754 bak-ex.sh "${PREFIX}/bin/bak-$(hostname).sh"
+install -Dm0644 bak-ex.filter "${shareDir}/bak/bak-$(hostname).filter"
+install -Dm0754 bak-log.sh "${PREFIX}/bin/bak-log.sh"
+install -Dm0754 rsync-errors.sh "${PREFIX}/bin/rsync-errors.sh"
 install -Dm0644 "LICENSE" "${shareDir}/doc/LICENSE"
 install -Dm0644 "README.md" "${shareDir}/doc/README.md"
 
